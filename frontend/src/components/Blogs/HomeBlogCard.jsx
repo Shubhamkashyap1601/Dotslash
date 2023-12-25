fcbba668f18ff75c5f9049b2b766c4b6b00af778
vasu52 <vkapasiya5252@gmail.com>
2023-12-25 22:09:07 +0530

    Home Blog Card Edited

--- frontend/src/components/Blogs/HomeBlogCard.jsx
+++ frontend/src/components/Blogs/HomeBlogCard.jsx
@@ -5,25 +5,27 @@ function HomeBlogCard({blog}) {
     <>
         <div className="blog-card">
 
-            <div className="blog-card-body">
-                <div className="blog-card-body-image">
-                    <img src={blog.image} alt="" />    
-                </div>
-                <p>{blog.description}</p>
-                
+            <div className="blog-card-image">
+                <img src={blog.image} alt="" />    
             </div>
 
             <div className="blog-card-title">
-                {blog.title}
-            </div> 
+                <b>{blog.title}</b>
+            </div>
 
+            <div className="blog-card-body">
+                <p>{blog.description}</p>
+            </div>
             <div className="blog-card-footer">
+
                 <div className="blog-card-likes-and-date">
                     {blog.date} &#8729; Likes: {blog.likes}
                 </div>
+
                 <div className="blog-card-author">
                     <h4>{blog.author}</h4>
                 </div>
+
             </div>
 
         </div>
