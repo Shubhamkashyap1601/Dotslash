b7ea8916a46ee2e71b684976903e3f383379c598
mudit-loya <muditloya24@gmail.com>
2024-01-04 19:51:58 +0530

    Changed to main.jsx from Layout.jsx

--- backend/src/routes/student.routes.js
+++ backend/src/routes/student.routes.js
@@ -1,19 +1,18 @@
-import {Router} from "express"
-import { registerStudent,loginStudent,deleteStudent,logoutStudent,refreshAccessToken,getUser,isAuth} from "../controllers/student.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import { Router } from "express"
+import { registerStudent, loginStudent, logoutStudent, refreshAccessToken, getUser, isAuthorized } from "../controllers/student.controller.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js"
 import { fetchLikedBlogs } from "../controllers/blog.controller.js";
 
 
 const router = Router();
 
-router.route("/register").post(upload.single('pfp'),registerStudent);
+router.route("/register").post(upload.single('pfp'), registerStudent);
 router.route("/login").post(loginStudent);
 router.route("/logout").post(verifyJWT, logoutStudent);
-// router.route("/delete").post(deleteStudent);
-
 router.route("/refreshAccessToken").post(refreshAccessToken);
 router.route("/user/:username").get(getUser);
 router.route("/user/:username/liked").get(fetchLikedBlogs);
-router.route("/auth").get(verifyJWT,isAuth)
+router.route("/authorized").get(verifyJWT, isAuthorized)
+
 export default router;
