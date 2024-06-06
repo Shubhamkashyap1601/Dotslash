6fd0d7038f7b9892c368abb3e21afd46eb753138
Mudit Loya <muditloya24@gmail.com>
2024-06-06 09:11:21 +0530

    CSS changes for Resources tables

--- frontend/src/components/Resources/ResourcesDetails.jsx
+++ frontend/src/components/Resources/ResourcesDetails.jsx
@@ -3,22 +3,25 @@ import React from 'react'
 const ResourcesDetails = ({resources}) => {
     return(
         <>
-        <table className="resource-table">
+        <table>
             <thead>
                 <tr>
                     <th>Resource</th>
-                    <th>Link</th>
+                    <th>Author/<br/> Creator</th>
                 </tr>
             </thead>
-            <tbody className="table-body">
+            <tbody>
                 {resources.map((resource, index) => (
                     <tr key={index}>
-                        <td className="resource-data">
+                        <td className="resource">
+                            <a href={resource.link} target="_blank">
                             <div className="resource-title">{resource.title}</div>
                             <div className="resource-description">{resource.description}</div>
+                            </a>
                         </td>
-                        <td className="resource-link"><a href={resource.link}>{resource.type}</a></td>
-                    </tr>))}
+                        <td className="resource-owner">{resource.owner}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
         </>
