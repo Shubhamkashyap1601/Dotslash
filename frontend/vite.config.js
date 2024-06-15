19df91ea5745165dad99ff21730be65b8e021ff4
daulatojha17 <daulatojha17@gmail.com>
2024-06-15 09:09:25 +0530

    fixed fetching URL from .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_URI,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_BACKEND_URI ?? 'http://localhost:3000'}`;
+  const PORT = `${env.VITE_PORT ?? '3000'}`;
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
+      port: PORT,
     },
-  },
-  plugins: [react()],
+    build: {
+      outDir: 'public',
+    },
+    plugins: [react()],
+  };
 });
-  
